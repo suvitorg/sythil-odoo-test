9f80acf9083a68a4b310e7d99603d1a212074f27
SythilBlade <stevewright2009@gmail.com>
2015-05-10 13:46:06 +1000

    add image
    memo to self, test these things locally so I don't go through 1,000
    commits

--- australia_states_territories/__openerp__.py
+++ australia_states_territories/__openerp__.py
@@ -2,7 +2,7 @@
     'name': "Australia States and Territories",
     'version': "1.1",
     'author': "Anonymous",
-    'category': "module_category_localization",
+    'category': "Localization",
     'depends': ['crm'],
     'data': ['res.country.state.csv'],
     'demo': [],
