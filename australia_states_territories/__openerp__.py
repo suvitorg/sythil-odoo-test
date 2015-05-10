16f4daa727b7e046a79e13163ffa920937069206
SythilBlade <stevewright2009@gmail.com>
2015-05-10 13:10:04 +1000

    first commit
    adds Australia states and territories to odoo

---
+++ australia_states_territories/__openerp__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Australia States and Territories",
+    'version': "1.1",
+    'author': "Anonymous",
+    'category': "module_category_localization",
+    'depends': ['crm'],
+    'data': ['res.country.state.csv'],
+    'demo': [],
+    'installable': True,
+}
