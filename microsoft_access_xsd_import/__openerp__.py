2367da691accc185f04347bf610931ef00d0dec0
SythilBlade <stevewright2009@gmail.com>
2015-06-24 21:35:25 +1000

    added maxsdi module
    First version still could have 100 features added

---
+++ microsoft_access_xsd_import/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Microsoft Access XSD Import",
+    'version': "1.0",
+    'author': "Sythil",
+    'category': "Tools",
+    'data': [
+        'maxsdi.xml',
+    ],
+    'demo': [],
+    'depends': [],
+    'installable': True,
+}
