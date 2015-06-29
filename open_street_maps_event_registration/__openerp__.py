2eeae9ae9b4f5d685c602c44b31cc2ce2d7c4522
SythilBlade <stevewright2009@gmail.com>
2015-06-29 12:48:52 +1000

    Module that displays event registrants on map
    Module that displays event registrants on map

---
+++ open_street_maps_event_registration/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Open Street Maps Event Registrations",
+    'version': "1.0",
+    'author': "Sythil",
+    'category': "Tools",
+    'data': [
+        'osmer.xml',
+    ],
+    'demo': [],
+    'depends': ['event'],
+    'installable': True,
+}
---
+++ open_street_maps_event_registration/osmer.py
@@ -0,0 +1,86 @@
+from openerp import models, fields, api
+
+import logging
+_logger = logging.getLogger(__name__)
+
+import werkzeug
+import werkzeug.urls
+import random
+
+from openerp import http, SUPERUSER_ID
+from openerp.http import request
+from openerp.tools.translate import _
+from geopy.geocoders import Nominatim
+
+class osmer(models.Model):
+
+    _inherit = "event.event"
+
+    osmer_secret = fields.Char()
+    osmer_url = fields.Char(string="Map URL")
+ 
+    @api.one
+    def open_map(self):
+        
+        rand_number = random.randint(1, 1000000)
+        
+        self.write({'osmer_secret':rand_number,'osmer_url':request.httprequest.host_url + "osmer_map?secret=" + str(rand_number)})
+        
+
+class MyController(http.Controller):
+
+    @http.route('/osmer_map',type="http")
+    def some_code(self, **kwargs):
+        
+        values = {}
+	for field_name, field_value in kwargs.items():
+            values[field_name] = field_value
+        
+        my_string = ""
+        my_string += "<link rel=\"stylesheet\" href=\"http://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.css\" />\n"
+        my_string += "<script src=\"http://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.js\"></script>\n"
+        my_string += "<div id=\"map\" style=\"width: 400px; height: 400px\"></div>\n"
+	my_string += "<script>\n"
+	my_string += "var map = L.map('map').setView([-37.82118, 144.96323], 13);\n"
+	my_string += "L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {\n"
+	my_string += "maxZoom: 18,\n"
+	my_string += "attribution: 'Map data copyright <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, ' +\n"
+	my_string += "'<a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, ' +\n"
+	my_string += "'Imagery copyright <a href=\"http://mapbox.com\">Mapbox</a>',\n"
+	my_string += "id: 'examples.map-i875mjb7'\n"
+	my_string += "}).addTo(map);\n"
+
+        my_events_ids = request.registry['event.event'].search(request.cr, SUPERUSER_ID, [('osmer_secret','=',values['secret'])],limit=1)
+	
+	if len(my_events_ids) == 0:
+	    return "Map does not exist"
+	my_events = request.registry['event.event'].browse(request.cr, SUPERUSER_ID, my_events_ids)
+        my_events[0].osmer_secret = ""
+        my_events[0].osmer_url = ""
+
+        geolocator = Nominatim()
+        for reg in my_events[0].registration_ids:
+            location_string = ""
+            
+            if reg.partner_id.street != False:
+                location_string += reg.partner_id.street
+            
+            if reg.partner_id.city != False:
+                location_string += ", " + reg.partner_id.city
+            
+            if reg.partner_id.state_id.name != False:
+                location_string += ", " + reg.partner_id.state_id.name
+            
+            if reg.partner_id.country_id.name != False:
+                location_string += ", " + reg.partner_id.country_id.name
+            
+            try:
+                location = geolocator.geocode(location_string)
+                my_string += "L.marker([" + str(location.latitude) + ", " + str(location.longitude) + "]).addTo(map).bindPopup(\"<b>" + reg.partner_id.name + "</b><br>" + str(location_string) + "\").openPopup();\n"
+            except:
+                _logger.error("Failed to geocode")
+
+	my_string += "</script>"
+        return my_string
+        
+        
