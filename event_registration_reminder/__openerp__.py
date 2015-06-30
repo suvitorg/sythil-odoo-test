07e92b21523ec420407aa136d0c294c00262f90a
SythilBlade <stevewright2009@gmail.com>
2015-06-30 14:12:34 +1000

    module that sends out event reminders to all registrants
    module that sends out event reminders to all registrants

---
+++ event_registration_reminder/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Event Registration Reminder",
+    'version': "1.0",
+    'author': "Sythil",
+    'category': "Tools",
+    'data': [
+        'ir.cron.csv',
+        'email.template.csv',
+    ],
+    'demo': [],
+    'depends': ['event'],
+    'installable': True,
+}
