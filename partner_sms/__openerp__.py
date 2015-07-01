870daad36bf805027b55fbfa813099fb660d9955
SythilBlade <stevewright2009@gmail.com>
2015-07-01 23:31:14 +1000

    module to send smses to partners
    module to send smses to partners

---
+++ partner_sms/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Partner SMS (SMSGLOBAL)",
+    'version': "1.0",
+    'author': "Sythil",
+    'category': "Tools",
+    'summary': "Allows you send smses to partners",
+    'data': [
+        'psms.xml',
+    ],
+    'demo': [],
+    'depends': [],
+    'installable': True,
+}
---
+++ partner_sms/psms.py
@@ -0,0 +1,71 @@
+from openerp import models, fields, api
+import logging
+_logger = logging.getLogger(__name__)
+import requests
+from datetime import datetime
+
+class psms_conf(models.Model):
+
+    _name = "psms.conf"
+    
+    name = fields.Char(required=True, string='Gateway Name')
+    username = fields.Char(required='True', string='API Username')
+    password = fields.Char(required='True', string='API Password')
+    from_number = fields.Char(required='True', string='From Number')
+
+class psms_compose(models.TransientModel):
+
+    _name = "psms.compose"
+    
+    record_id = fields.Integer()
+    model_id = fields.Char()
+    sms_gateway = fields.Many2one('psms.conf', required=True, string='Account/Number')
+    to_number = fields.Char(required=True, string='To Mobile Number', readonly=True)
+    sms_content = fields.Text('SMS Content')
+    
+    @api.one
+    def send_sms(self):
+    
+        format_number = self.to_number
+        format_number = format_number.replace(" ", "")
+        format_number = format_number.replace("+", "")
+        
+        smsglobal_url = "http://www.smsglobal.com/http-api.php?action=sendsms&user=" + self.sms_gateway.username + "&password=" + self.sms_gateway.password + "&from=" + self.sms_gateway.from_number + "&to=" + format_number + "&text=" + self.sms_content
+        r = requests.get(smsglobal_url)
+        
+        my_model = self.env['ir.model'].search([('model','=',self.model_id)])
+        psms_history = self.env['psms.history'].create({'record_id': self.record_id,'model_id':my_model[0].id,'from_mobile':self.sms_gateway.from_number,'to_mobile':self.to_number,'sms_content':self.sms_content,'status_string':r.text, 'direction':'O','my_date':datetime.utcnow()})
+
+class psms_partner(models.Model):
+
+    _inherit = "res.partner"
+    
+    smshistory = fields.Many2many('psms.history')
+     
+class psms_history(models.Model):
+
+    _name = "psms.history"
+    
+    record_id = fields.Integer(readonly=True)
+    model_id = fields.Many2one('ir.model', readonly=True)
+    from_mobile = fields.Char(string="From Mobile Number", readonly=True)
+    to_mobile = fields.Char(string="To Mobile Number", readonly=True)
+    sms_content = fields.Text(string="SMS Message", readonly=True)
+    record_name = fields.Char(string="Record Name", compute="_rec_nam")
+    status_string = fields.Char(string="Status Code", readonly=True)
+    direction = fields.Selection((("I","INBOUND"),("O","OUTBOUND")), string="Status Code", readonly=True)
+    my_date = fields.Datetime(string="Date", readonly=True, help="The date and time the sms is received or sent")
+    status_mini = fields.Char(compute="_short_status", string="Status")
+
+
+    @api.one
+    @api.depends('record_id', 'model_id')
+    def _rec_nam(self):
+        my_record = self.env[self.model_id.model].search([('id','=',self.record_id)])
+        self.record_name = my_record.name
+        
+
+    @api.one
+    @api.depends('status_string')
+    def _short_status(self):
+        self.status_mini = self.status_string.split(';', 1)[0];
