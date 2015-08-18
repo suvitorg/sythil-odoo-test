a81a5d69654590837c664b47cbf308971f8ac0e0
Alexei Gradinari <alex2grad@gmail.com>
2015-08-18 12:02:47 -0400

    added access models permissions

--- entity_sms/__openerp__.py
+++ entity_sms/__openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Entity SMS (SMSGATEWAY,CLICKATELL,SMSGLOBAL)",
-    'version': "1.2",
+    'version': "1.3",
     'author': "Sythil",
     'category': "Tools",
     'summary': "Allows you send smses from any model",
@@ -8,6 +8,7 @@
         'views/qweb.xml',
         'esms.xml',
         'esms.gateways.csv',
+	'security/ir.model.access.csv',
         'smsgateway/gateway_config.xml',
         'clickatell/gateway_config.xml',
         'smsglobal/gateway_config.xml',
