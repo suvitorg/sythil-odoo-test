3c2522b6e09b1de26cc902db02fdbdce003d00a0
SythilBlade <stevewright2009@gmail.com>
2015-09-26 07:53:08 +1000

    make it so you can only select fields from selected model
    make it so you can only select fields from selected model

--- entity_html_form/__openerp__.py
+++ entity_html_form/__openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Entity HTML Form Generator",
-    'version': "1.0",
+    'version': "1.1",
     'author': "Sythil",
     'category': "Tools",
     'summary': "Creates an HTML form that you can embed anywhere",
--- entity_html_form/entity_html_form.py
+++ entity_html_form/entity_html_form.py
@@ -14,6 +14,7 @@ class ehtml_gen(models.Model):
     required_fields = fields.Text(readonly=True)
     defaults_values = fields.One2many('ehtml.fielddefault', 'html_id', string="Default Values", help="Sets the value of an field before it gets inserted into the database")
     return_url = fields.Char(string="Return URL", help="The URL that the user will be redirected to after submitting the form", required=True)
+    form_type = fields.Selection([('reg','Plain'),('odoo','Odoo Website')], default="odoo", string="Form Type")
     
     @api.onchange('model_id')
     @api.one
@@ -30,9 +31,17 @@ class ehtml_gen(models.Model):
             if (model_field.required == True):
                 self.required_fields += str(model_field.field_description) + " (" + str(model_field.name) + ")\n"
         
-        
     @api.one
     def generate_form(self):
+        if self.form_type == 'reg':
+            self.generate_form_reg()
+        elif self.form_type == 'odoo':
+            self.generate_form_odoo()
+        else:
+            self.generate_form_optimize()
+            
+    @api.one
+    def generate_form_reg(self):
         html_output = ""
         html_output += "<link href=\"http://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css\" rel=\"stylesheet\">\n";
 	html_output += "<script src=\"http://code.jquery.com/jquery-1.10.2.js\"></script>\n";
@@ -99,16 +108,101 @@ class ehtml_gen(models.Model):
     	    
     	    html_output += "<br>\n"
     	html_output += '<input type="hidden" name="form_id" value="' + str(self.id) + '"/>' + "\n"
-    	html_output += '<input type="submit" value="Submit Forms"/>' + "\n"
+    	html_output += '<input type="submit" value="Submit Form"/>' + "\n"
     	html_output += "</form>\n"
         html_output += "</div>"
         self.output_html = html_output
-        
+       
+    @api.one
+    def generate_form_odoo(self):
+        html_output = ""
+        html_output += '<div id="ehtml_form">' + "\n"
+        html_output += '<form method="POST" class="form-horizontal mt32" action="' + request.httprequest.host_url + 'form/myinsert">' + "\n"
+        for fe in self.fields_ids:              
+            html_output += "<div t-attf-class=\"form-group #{error and 'name' in error and 'has-error' or ''}\">\n"
+	    html_output += "<label class=\"col-md-3 col-sm-4 control-label\" for=\"" + fe.html_name + "\">" + fe.field_id.field_description
+            
+                
+            if fe.field_id.required == True:
+                html_output += ' *'
+                
+            html_output += '</label>\n'
+            
+            html_output += "<div class=\"col-md-7 col-sm-8\">\n";
+            
+            if fe.html_field_type == "text":
+                html_output += '<input type="text" class="form-control" id="' + fe.html_name + '" name="' + fe.html_name + '"'
+                    
+                if fe.field_id.size > 0:
+                    html_output += ' maxlength="' + fe.field_id.size + '"'
+                    
+                if fe.field_id.required == True:
+                    html_output += ' required="required"'
+                
+                html_output += '/>\n'
+                    
+            if fe.html_field_type == "textarea":
+                html_output += '<textarea class="form-control" id="' + fe.html_name + '" name="' + fe.html_name + '"'
+                   
+                if fe.field_id.required == True:
+    	            html_output += ' required'
+    	            
+    	        html_output += '></textarea>\n'
+    	        
+    	    if fe.html_field_type == "number":
+	        html_output += '<input type="number" id="' + fe.html_name + '" name="' + fe.html_name + '"'
+		    
+	        if fe.field_id.required == True:
+	            html_output += ' required'        
+		    	            
+    	        html_output += '/>\n'
+    	    
+    	    if fe.html_field_type == "search":
+                html_output += "<script>\n"
+                html_output += "$(document).ready(function() {\n"
+                html_output += '    $("#' + fe.html_name + '").autocomplete({' + "\n"
+                html_output += "        source: function( request, response ) {\n"
+                html_output += '            $.ajax({url: "' + request.httprequest.host_url + 'form/autocomplete?callback=?",dataType: "jsonp",' + "\n"
+                html_output += '            data: {'+ "\n"
+                html_output += "                q: request.term\n"
+                html_output += "            },\n"
+                html_output += "            success: function( data ) {\n"
+                html_output += "                response( data );\n"
+                html_output += "            }});\n"
+                html_output += "        }\n"
+                html_output += "    });\n"
+                html_output += "});\n"
+                html_output += "</script>\n"
+                html_output += '<input type="search" id="' + fe.html_name + '" name="' + fe.html_name + '"'
+		    
+	        if fe.field_id.required == True:
+	            html_output += ' required'        
+		    	            
+    	        html_output += '/>\n'
+    	    
+    	    
+	    html_output += "</div>\n"
+            html_output += "</div>\n"
+            
+            
+	html_output += "<div class=\"form-group\">\n"
+        html_output += "<div class=\"col-md-offset-3 col-sm-offset-4 col-sm-8 col-md-7\">\n"
+    	html_output += '<input type="hidden" name="form_id" value="' + str(self.id) + '"/>' + "\n"
+        html_output += "<input type=\"submit\" class=\"btn btn-primary btn-lg\" value=\"Send\"/>\n"
+        html_output += "</div>\n"
+        html_output += "</div>\n"
+
+    	html_output += "</form>\n"
+        html_output += "</div>"
+        self.output_html = html_output
+
 class ehtml_field_entry(models.Model):
 
     _name = "ehtml.fieldentry"
 
     html_id = fields.Many2one('ehtml.formgen')
+    model_id = fields.Many2one('ir.model', string="Model", required=True)
+    model = fields.Char(related="model_id.model")
     field_id = fields.Many2one('ir.model.fields', domain="[('name','!=','create_date'),('name','!=','create_uid'),('name','!=','id'),('name','!=','write_date'),('name','!=','write_uid')]", string="Form Fields")
     html_name = fields.Char(string="HTML Field Name")
     html_field_type = fields.Selection((('text','Textbox'),('textarea','Textarea'),('number','Number'), ('search','Search') ), string="HTML Field Type")
@@ -135,6 +229,8 @@ class ehtml_field_default(models.Model):
     _name = "ehtml.fielddefault"
 
     html_id = fields.Many2one('ehtml.formgen')
+    model_id = fields.Many2one('ir.model', string="Model", required=True)
+    model = fields.Char(related="model_id.model")
     field_id = fields.Many2one('ir.model.fields', string="Form Fields")
     default_value = fields.Char(string="Default Value")
     
