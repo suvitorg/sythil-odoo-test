6a2903c2234cc227f1725137214c4cefa52e486f
SythilBlade <stevewright2009@gmail.com>
2015-10-21 19:44:23 +1100

    What a stupid module...
    What a stupid module...
    won't bother upgrading this to v9 chances are I'll eventually just
    delete it.

--- event_registration_reminder/__openerp__.py
+++ event_registration_reminder/__openerp__.py
@@ -1,8 +1,9 @@
 {
-    'name': "Event Registration Reminder",
+    'name': "(obsolete)Event Registration Reminder",
     'version': "1.0",
     'author': "Sythil",
     'category': "Tools",
+    'summary':'(obsolete)Sends an email out a few hours before an event',
     'data': [
         'ir.cron.csv',
         'email.template.csv',
