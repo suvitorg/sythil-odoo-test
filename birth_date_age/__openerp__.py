2ea49b32c7415332f1205be52380c2fea48a451e
SythilBlade <stevewright2009@gmail.com>
2015-10-21 19:40:24 +1100

    updating age module
    updating age module
    
    requested from friend, still pruning esms of bugs...

---
+++ birth_date_age/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Birth Date Age",
+    'version': "1.0",
+    'author': "Sythil",
+    'category': "Tools",
+    'summary':'Adds Date of Birth and Age which is updated daily',
+    'data': [
+        'bda.xml',
+        'ir.cron.csv',
+    ],
+    'demo': [],
+    'depends': [],
+    'installable': True,
+}
