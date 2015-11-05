f8e2a139d39304ef346eb82bb5bb06798a384a57
SythilBlade <stevewright2009@gmail.com>
2015-11-05 18:31:29 +1100

    ERD Maker
    ERD Maker
    Drawing the lines between the tables would be a lot of effort...

---
+++ erd_maker/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Entity Relationship Diagram(ERD) Maker",
+    'version': "1.0",
+    'author': "Sythil",
+    'category': "Tools",
+    'summary':'Creates an ERD starting from the specified table and transending a certain depth',
+    'data': [
+        'erdm.xml',
+    ],
+    'demo': [],
+    'depends': [],
+    'installable': True,
+}
