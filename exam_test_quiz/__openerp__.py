d3ebe5cf9b8b23ae403b6635a52f81ab396de4a4
SythilBlade <stevewright2009@gmail.com>
2015-11-17 19:46:40 +1100

    Share Exam Feature
    Share Exam Feature

--- exam_test_quiz/__openerp__.py
+++ exam_test_quiz/__openerp__.py
@@ -1,13 +1,15 @@
 {
     'name': "Online Exam / Test / Quiz",
-    'version': "0.6",
+    'version': "0.6.1",
     'author': "Sythil",
     'category': "Tools",
     'summary': "Informal Online Exam / Test / Quiz",
     'data': [
+        'email.template.csv',
         'etq_exam.xml',
         'views/exam_templates.xml',
         'etq_results.xml',
+        'etq_exam_share.xml',
     ],
     'demo': [],
     'depends': ['website'],
