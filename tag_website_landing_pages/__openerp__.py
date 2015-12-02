8712d13f285dd9d7cbbbee76b4d0d30c21273c71
SythilBlade <stevewright2009@gmail.com>
2015-12-02 15:12:23 +1100

    works odoo account is err broken...
    works odoo account is err broken...

---
+++ tag_website_landing_pages/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Website Landing Page(TAG)",
+    'version': "1.1",
+    'author': "TAGSBM",
+    'category': "Tools",
+    'data': [
+        'views/landing_page.xml',
+    ],
+    'demo': [],
+    'depends': ['website'],
+    'installable': True,
+}
