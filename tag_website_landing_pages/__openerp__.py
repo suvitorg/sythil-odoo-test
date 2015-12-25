955a209ecca1d702db88d1b6b5aa5f3badf2f671
SythilTech <stevewright2009@gmail.com>
2015-12-25 16:49:23 +1100

    seriously Odoo let me remove modules from the store...
    seriously Odoo let me remove modules from the store...

---
+++ tag_website_landing_pages/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Dead Module",
+    'version': "1.0",
+    'author': "Dead",
+    'category': "Tools",
+    'summary': "The orginal module had bugs which screwed up your system....",
+    'data': [
+    ],
+    'demo': [],
+    'depends': ['web', 'crm', 'website'],
+    'images':[],
+    'installable': True,
+}
