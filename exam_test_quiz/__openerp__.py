5fe391cc34f7f3fa4d0613964ef7bbd7f54be57a
SythilTech <stevewright2009@gmail.com>
2016-04-22 07:31:15 +1000

    need coffee before I start coding...
    need coffee before I start coding...

--- exam_test_quiz/__openerp__.py
+++ exam_test_quiz/__openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Online Exam / Test / Quiz",
-    'version': "1.0",
+    'version': "1.0.1",
     'author': "Sythil",
     'category': "Tools",
     'summary': "Informal Online Exam / Test / Quiz",
